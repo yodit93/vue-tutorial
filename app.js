ab83ea0c9da33e8db87f4811536a49d7ee8940e2
yodit93 <yoditabebe9391@gmail.com>
2023-10-07 17:18:47 +0300

    Mouse events

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const app = Vue.createApp({
             showBooks: true,
             title: "Hello Vue.js",
             author: "Yodit",
-            age: 31
+            age: 31,
+            x: 0,
+            y: 0
         }
     },
     // component method using method property, any method should be placed here
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e)
+            if(data) {
+                console.log(data);
+            }
+        },
+        handleMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
